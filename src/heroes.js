668f084a98b36fe3d221b4638910c9112f0c94b1
Ramon Ferreira <rferreira_22@yahoo.com.br>
2021-05-10 20:15:02 -0300

    Adicionados assets para icones dos herois

--- src/heroes.js
+++ src/heroes.js
@@ -4,19 +4,61 @@ export const heroes = [{
     img: "assets/alencia-icon.png",
     rarity: 5,
     element: "earth",
-    role: "warrior"
+    role: "Warrior"
 },{
     id: 2,
     name: "Tywin Ambicioso",
     img: "assets/atywin-icon.png",
     rarity: 5,
     element: "light",
-    role: "knight"
+    role: "Knight"
 },{
     id: 3,
     name: "Ravi Apocalíptica",
     img: "assets/aravi-icon.png",
     rarity: 5,
     element: "dark",
-    role: "warrior"
+    role: "Warrior"
+},{
+    id: 4,
+    name: "Araminta",
+    img: "https://epic7x.com/wp-content/uploads/2019/01/Aramintha-icon.png",
+    rarity: 5,
+    element: "fire",
+    role: "Mage"
+},{
+    id: 5,
+    name: "Árbitro Vildred",
+    img: "https://epic7x.com/wp-content/uploads/2019/01/Arbiter-Vildred-icon-1.png",
+    rarity: 5,
+    element: "dark",
+    role: "Thief"
+},{
+    id: 6,
+    name: "Sombra do Arquidemônio",
+    img: "https://epic7x.com/wp-content/uploads/2021/01/Archdemon-Shadow-icon.png",
+    rarity: 5,
+    element: "dark",
+    role: "Mage"
+},{
+    id: 7,
+    name: "Baal e Sezan",
+    img: "https://epic7x.com/wp-content/uploads/2019/04/Baal-icon.png",
+    rarity: 5,
+    element: "fire",
+    role: "Mage"
+},{
+    id: 8,
+    name: "Baiken",
+    img: "https://epic7x.com/wp-content/uploads/2019/04/Baiken-Icon.png",
+    rarity: 5,
+    element: "earth",
+    role: "Thief"
+},{
+    id: 9,
+    name: "Bassar",
+    img: "https://epic7x.com/wp-content/uploads/2019/04/Basar-icon.png",
+    rarity: 5,
+    element: "earth",
+    role: "Mage"
 },]
